9e191dcaeb012391f1fecc6f475eea2db5fa9e09
Jordan Knight <jorkni@microsoft.com>
2017-02-05 12:53:00 +1100

    Added task extensions

---
+++ src/ExtensionGoo.Standard/Extensions/TaskExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionGoo.Standard.Extensions
+{
+    public static class TaskExtensions
+    {
+        public static Task WhenAllList<T>(this List<T> list, Func<T, Task> func)
+        {
+            return Task.WhenAll(list.Select(func).ToList());
+        }
+
+        public static async Task<IList<TResult>> WhenAllEnum<T, TResult>(this IEnumerable<T> list, Func<T, Task<TResult>> func)
+        {
+            return await Task.WhenAll(list.Select(func));
+        }
+    }
+}
